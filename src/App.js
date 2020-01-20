6d04f795d02b22a9fc35b516a767261aff98b0fa
Marina <mmolostvova@gmail.com>
2020-01-20 14:02:24 +0200

    Added toggle buttons

--- src/App.js
+++ src/App.js
@@ -1,15 +1,79 @@
 import React from 'react';
+import ColoredText from './colored-text';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    console.log('constructor #1');
+
+    this.state = {
+      counter: 1
+    };
+
+    this.onAddLogo = this.onAddLogo.bind(this);
+    this.onRemoveLogo = this.onRemoveLogo.bind(this);
+  }
+  
+  componentDidMount() {
+    console.log('componentDidMount #3')
+  }
+
+  onAddLogo() {
+    console.log('onAddLogo')
+    console.log(this.state)
+    this.setState((state) => {
+      return { counter: state.counter + 1 };
+    });
+  }
+
+  onRemoveLogo() {
+    console.log('onRemoveLogo')
+    console.log(this.state)
+    if (this.state.counter > 1) {
+      this.setState((state) => {
+        return { counter: state.counter - 1 };
+      })
+    } else {
+      alert('You can\'t remove last logo')
+    }
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('shouldComponentUpdate #4', nextProps, nextState)
+    if (this.state.counter === nextState.counter) {
+      return false 
+    } else {
+      return true
+    }
+  }
+
+  componentDidUpdate() {
+    console.log('componentDidUpdate #5');
+  }
+
+  render () {
+    console.log('render #2')
+
+    const renderedLogos = [];
+
+    for(let i = 0; i < this.state.counter; i++) {
+      renderedLogos.push(<img src={logo} className="App-logo" alt="logo" />);
+      console.log('------------#7')
+    }
+    
+    return (
+      
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {renderedLogos}
+        <ColoredText
+          className="text yellow-text"/>
+        <ColoredText
+          className="text green-text"/>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,9 +82,18 @@ function App() {
         >
           Learn React
         </a>
+      <button 
+        className="btn"
+        onClick={this.onAddLogo}>
+          Add logo
+      </button>
+      <button 
+        className="btn delete-btn"
+        onClick={this.onRemoveLogo}>
+          Remove logo
+      </button>
       </header>
     </div>
   );
 }
-
-export default App;
+}
---
+++ src/colored-text.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class ColoredText extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello, I'm the Text</h1>
+            </div>
+        )
+    }
+}
