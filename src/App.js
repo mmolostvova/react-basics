0ea7a4aae2a58c5ecf195862b838887c8b87a25d
Marina <mmolostvova@gmail.com>
2020-03-05 11:22:24 +0200

    Created Counter component outside of App.js

--- src/App.js
+++ src/App.js
@@ -1,76 +1,29 @@
-import React from 'react';
-import BigForm from './BigForm/BigForm';
+import React from "react";
+import BigForm from "./BigForm/BigForm";
+import Counter from "./Counter/Counter";
 
-//import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 export default class App extends React.Component {
-
   constructor(props) {
     super(props);
-    console.log('constructor #1');
-
-    this.state = {
-      counter: 1
-    };
-
-    this.onAddLogo = this.onAddLogo.bind(this);
-    this.onRemoveLogo = this.onRemoveLogo.bind(this);
+    console.log("constructor #1");
   }
-  
+
   componentDidMount() {
-    console.log('componentDidMount #3')
+    console.log("componentDidMount #3");
   }
 
-  onAddLogo() {
-    console.log('onAddLogo')
-    console.log(this.state)
-    this.setState((state) => {
-      return { counter: state.counter + 1 };
-    });
-  }
 
-  onRemoveLogo() {
-    console.log('onRemoveLogo')
-    console.log(this.state)
-    if (this.state.counter > 1) {
-      this.setState((state) => {
-        return { counter: state.counter - 1 };
-      })
-    } else {
-      alert('You can\'t remove last logo')
-    }
-  }
+  render() {
+    console.log("render #2");
 
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log('shouldComponentUpdate #4', nextProps, nextState)
-    if (this.state.counter === nextState.counter) {
-      return false 
-    } else {
-      return true
-    }
-  }
-
-  componentDidUpdate() {
-    console.log('componentDidUpdate #5');
-  }
-
-  render () {
-    console.log('render #2')
-
-    // const renderedLogos = [];
-
-    // for(let i = 0; i < this.state.counter; i++) {
-    //   renderedLogos.push(<img src={logo} className="App-logo-active" alt="logo" />);
-    //   console.log('------------#7')
-    // }
-    
     return (
-      
-    <div className="App">
-        <BigForm/>
-    </div>
-  );
-}
+      <div className="App">
+        <BigForm />
+        <Counter />
+      </div>
+    );
+  }
 }
---
+++ src/Counter/Counter.js
@@ -0,0 +1,78 @@
+import React from "react";
+
+import heart from "./heart.svg";
+
+import "./Counter.css";
+import { Button } from "react-bootstrap";
+
+export default class Counter extends React.Component {
+    constructor(props) {
+      super(props);
+      console.log("constructor #1");
+  
+      this.state = {
+        counter: 1
+      };
+  
+      this.onAddLogo = this.onAddLogo.bind(this);
+      this.onRemoveLogo = this.onRemoveLogo.bind(this);
+    }
+  
+    componentDidMount() {
+      console.log("componentDidMount #3");
+    }
+  
+    onAddLogo() {
+      console.log("onAddLogo");
+      console.log(this.state);
+      this.setState(state => {
+        return { counter: state.counter + 1 };
+      });
+    }
+  
+    onRemoveLogo() {
+      console.log("onRemoveLogo");
+      console.log(this.state);
+      if (this.state.counter > 1) {
+        this.setState(state => {
+          return { counter: state.counter - 1 };
+        });
+      } else {
+        alert("You can't remove last heart");
+      }
+    }
+  
+    shouldComponentUpdate(nextProps, nextState) {
+      console.log("shouldComponentUpdate #4", nextProps, nextState);
+      if (this.state.counter === nextState.counter) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+  
+    render() {
+      console.log("render #2");
+  
+      const renderedLogos = [];
+  
+      for (let i = 0; i < this.state.counter; i++) {
+        renderedLogos.push(
+          <img src={heart} className="App-logo-active" alt="logo" />
+        );
+        console.log("------------#7");
+      }
+  
+      return (
+          <div className="counter">
+            {renderedLogos}
+            <Button variant="success" onClick={this.onAddLogo}>
+              Add heart
+            </Button>
+            <Button variant="danger" onClick={this.onRemoveLogo}>
+              Remove heart
+            </Button>
+          </div>
+      );
+    }
+  }
